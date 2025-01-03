31b8cfb51154ac5d040c1583f799c6fa51fb3b0d
dimma-18 <Dimma-18@yandex.ru>
2025-01-04 02:06:05 +0300

    ci/cd MySQL

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -8,3 +8,10 @@ func Equal[T comparable](t *testing.T, actual, expected T) {
 		t.Errorf("got: %v; want: %v", actual, expected)
 	}
 }
+
+func NilError(t *testing.T, actual error) {
+	t.Helper()
+	if actual != nil {
+		t.Errorf("got: %v; expected: nil", actual)
+	}
+}
